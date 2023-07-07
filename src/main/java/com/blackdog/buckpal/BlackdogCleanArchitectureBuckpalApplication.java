d8c780580607e3298d4d97c783dfb3c13e94b898
흑구 <doqndnffo@gmail.com>
2023-07-07 18:06:01 +0900

    Initial commit

---
+++ src/main/java/com/blackdog/buckpal/BlackdogCleanArchitectureBuckpalApplication.java
@@ -0,0 +1,13 @@
+package com.blackdog.buckpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackdogCleanArchitectureBuckpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackdogCleanArchitectureBuckpalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackdog/buckpal/BlackdogCleanArchitectureBuckpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackdog.buckpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackdogCleanArchitectureBuckpalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
